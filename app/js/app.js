8bb1a73b60acb61c938b819a49629b030f706287
japguele <japguele@student.avans.nl>
2015-05-28 17:52:17 +0200

    created a board

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,19 @@ var boardController = require('./BoardController');
 var tileFactory =  require('./TileFactory')
 var app = angular.module('app', []);
 
+app.directive('tile', function(){
+return {
+	restrict: 'E',
+	templateUrl: './templates/tile.html',
+	controller: function($scope){
+
+	},
+	link : function(scope,element ,attrs){}
+
+
+}
+});
+
 
 app.factory('GamesFactory',gamesFactory);
 app.factory('TileFactory',tileFactory);
